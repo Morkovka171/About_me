9d2a57bf4c01ddc135f7abe5c624f1025259b723
Morkovka171 <guganowa@yandex.ru>
2022-11-17 14:55:13 +0300

    Бургер

--- main.js
+++ main.js
@@ -1,11 +1,11 @@
-const       burger = document.querySelector('.burger'),
-			close = document.querySelector('.menu_close'),
-			menu = document.querySelector('.menu');
+// const burger = document.querySelector('.burger'),
+// 			close = document.querySelector('.menu_close'),
+// 			menu = document.querySelector('.menu');
 
-            burger.addEventListener('click', () => {
-                menu.classList.add('menu--visible');
-            });
+//             burger.addEventListener('click', () => {
+//                 menu.classList.add('menu--visible');
+//             });
             
-            close.addEventListener('click', () => {
-                menu.classList.remove('menu--visible');
-            });
+//             close.addEventListener('click', () => {
+//                 menu.classList.remove('menu--visible');
+//             });
