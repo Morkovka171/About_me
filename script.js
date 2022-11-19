d6acdbcbaaca94906b50d0448223f5be679c3924
kolodidon <www.teteva@yandex.ru>
2022-11-19 13:39:14 +0300

    бургер

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Настройки для карусели
 $('.carousel__wrapper').slick({
     dots: true,
     speed: 500,
@@ -11,3 +12,20 @@ $('.carousel__wrapper').slick({
         }
       ]
 });
+
+// Настройка для бургера
+const burger = document.getElementById('burger');
+const toggleMenu = () => {
+  const menu = document.getElementById('menu');
+  const header = document.getElementById('header');
+
+  if (menu.classList.contains("opened")) {
+    menu.classList.remove("opened");
+    header.classList.remove("expanded");
+  } else {
+    menu.classList.add("opened");
+    header.classList.add("expanded");
+  }
+
+}
+burger.addEventListener('click', () => toggleMenu() )
